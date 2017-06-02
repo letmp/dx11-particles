b7789d7db37fdcd55627f1eb0cf4095d64341934
Marko Ritter <velcrome@gmx.net>
2017-06-02 02:59:27 +0200

    Update CameraSimpleNode.cs
    remove unused pin

--- src/Nodes/VVVV.Extensions/CameraSimpleNode.cs
+++ src/Nodes/VVVV.Extensions/CameraSimpleNode.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using VVVV.PluginInterfaces.V1;
 using VVVV.PluginInterfaces.V2;
 using VVVV.Utils.VMath;
@@ -6,21 +6,21 @@ using VVVV.Utils.VMath;
 namespace VVVV.Extensions
 {
     #region PluginInfo
-    [PluginInfo(Name = "Camera", Category = "Transform Basic", Help = "Does a Perspective Projection with Lens Shift", Tags = "Projector", Author = "velcrome")]
+    [PluginInfo(Name = "Camera", Category = "Transform Basic", Help = "Does a Perspective Projection with Lens Shift", Tags = "Projector, Beamer, Mapping", Author = "velcrome")]
     #endregion PluginInfo
     public class CameraSimpleNode : IPluginEvaluate
     {
         #region fields & pins
-        [Input("Translate", DefaultValues = new[] { 0.0, 0, -1 })]
+        [Input("Translate", DefaultValues = new[] { 0, 0, -1 })]
         public ISpread<Vector3D> FTranslate;
 
-        [Input("Rotate", DefaultValues = new[] { 0.0, 0, 0 })]
+        [Input("Rotate", DefaultValues = new[] { 0, 0, 0 })]
         public ISpread<Vector3D> FRotate;
 
-        [Input("Shift", DefaultValues = new[] { 0.0, 0 })]
+        [Input("Shift", DefaultValues = new[] { 0, 0 })]
         public ISpread<Vector2D> FShift;
 
-        [Input("FoV", DefaultValue = 0.2)]
+        [Input("FoV", DefaultValues = new[] {0.25, 0,140625})] // 16:9 as default 
         public ISpread<Vector2D> FFov;
 
         [Input("Near Plane", DefaultValue = 0.05, Visibility = PinVisibility.OnlyInspector)]
@@ -28,9 +28,6 @@ namespace VVVV.Extensions
 
         [Input("Far Plane", DefaultValue = 100, Visibility = PinVisibility.OnlyInspector)]
         public ISpread<double> FFar;
-
-        [Input("Aspect Ratio", DefaultValue = 0.75, Visibility = PinVisibility.OnlyInspector)]
-        public ISpread<double> FAspect;
 
         [Output("View")]
         public ISpread<Matrix4x4> FView;
@@ -43,7 +40,7 @@ namespace VVVV.Extensions
 
         #endregion fields & pins
 
-        //called when data for any output pin is requested
+// called every frame. computation could be triggered selectively on change for performance gains
         public void Evaluate(int SpreadMax)
         {
             FView.SliceCount =
@@ -60,7 +57,6 @@ namespace VVVV.Extensions
 
                 var near = FNear[i];
                 var far = FFar[i];
-                var aspect = FAspect[i];
 
                 var view = VMath.Inverse(VMath.Rotate(rotate * Math.PI * 2) * VMath.Translate(translate));
 
@@ -68,10 +64,12 @@ namespace VVVV.Extensions
                 double scaleY = 1.0 / Math.Tan(fov[1] * Math.PI);
                 double fn = far / (far - near);
 
-                var proj = new Matrix4x4(scaleX, 0, 0, 0,
-                                              0, scaleY, 0, 0,
-                                          -2 * shift.x, -2 * shift.y, fn, 1,
-                                              0, 0, -near * fn, 0);
+                var proj = new Matrix4x4(
+                          scaleX,            0,          0, 0,
+                               0,       scaleY,          0, 0,
+                    -2 * shift.x, -2 * shift.y,         fn, 1,
+                               0,            0, -near * fn, 0
+                          );
 
                 FView[i] = view;
                 FProjection[i] = proj;
