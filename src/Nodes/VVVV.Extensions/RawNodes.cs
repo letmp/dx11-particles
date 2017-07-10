9600916eff777fd890f7767de2a27af18c47342f
letmp <le_tmp@gmx.de>
2017-07-10 17:27:42 +0200

    [added] network support

---
+++ src/Nodes/VVVV.Extensions/RawNodes.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.IO;
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+
+namespace VVVV.Extensions
+{
+    #region PluginInfo
+    [PluginInfo(Name = "Splitter", Category = "Raw", Help = "Splits a raw stream in spreads with a given size.", Tags = "")]
+    #endregion PluginInfo
+    public class RawSplitterNode : IPluginEvaluate, IPartImportsSatisfiedNotification
+    {
+        #region fields & pins
+        [Input("Input")]
+        public ISpread<Stream> FInput;
+
+        [Input("Count", MinValue = 0, DefaultValue = 768)]
+        public IDiffSpread<int> FCount;
+
+        [Output("Output")]
+        public ISpread<ISpread<Stream>> FOutput;
+
+        //when dealing with byte streams (what we call Raw in the GUI) it's always
+        //good to have a byte buffer around. we'll use it when copying the data.
+        protected byte[] FBuffer;
+        #endregion fields & pins
+
+        //called when all inputs and outputs defined above are assigned from the host
+        public void OnImportsSatisfied()
+        {
+            //start with an empty stream output
+            FOutput.SliceCount = 0;
+            FCount.Changed += ResizeBuffer;
+        }
+
+        protected void ResizeBuffer(IDiffSpread<int> spread)
+        {
+            FBuffer = new byte[FCount[0]];
+        }
+
+
+        //called when data for any output pin is requested
+        public void Evaluate(int spreadMax)
+        {
+            spreadMax = FInput.SliceCount;
+            FOutput.SliceCount = spreadMax;
+
+            for (int i = 0; i < spreadMax; i++)
+            {
+                var input = FInput[i];
+                int count = (int)Math.Ceiling((double)input.Length / FCount[0]);
+
+                input.Position = 0;
+                FOutput[i].ResizeAndDispose(count, () => new MemoryStream(FCount[0]));
+
+                int length = (int)input.Length;
+                for (int j = 0; j < count; j++)
+                {
+                    var output = FOutput[i][j];
+                    output.Position = 0;
+
+                    var numBytesToCopy = Math.Min(length, FCount[0]);
+                    output.SetLength(numBytesToCopy);
+
+                    input.Read(FBuffer, 0, numBytesToCopy);
+                    output.Write(FBuffer, 0, numBytesToCopy);
+                    
+                    length -= numBytesToCopy;
+                }
+
+            }
+        }
+    }
+
+    
+}
---
+++ src/Nodes/VVVV.Extensions/StringSessionNode.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+
+namespace VVVV.Extensions
+{
+    #region PluginInfo
+    [PluginInfo(Name = "Indexer", Category = "String", AutoEvaluate = true, Help = "Session Management based on unique string-based Names. Outputs are useful for feeding a Buffer.", Tags = "Buffer", Author = "velcrome")]
+    #endregion PluginInfo
+    public class StringIndexerNode : IPluginEvaluate, IPartImportsSatisfiedNotification
+    {
+        #region fields & pins
+        [Input("Name", DefaultString = "id")]
+        public ISpread<string> FInput;
+
+        [Input("Remove Name", DefaultString = "")]
+        public ISpread<string> FRemove;
+
+        [Input("Timeout", IsSingle = true)]
+        public IDiffSpread<TimeSpan> FTimeOut;
+
+        [Input("Index Count", DefaultValue = 256, IsSingle = true)]
+        public IDiffSpread<int> FCount;
+
+        [Output("Index")]
+        public ISpread<int> FFormerId;
+
+        [Output("Fresh")]
+        public ISpread<int> FIdFresh;
+
+        [Output("Removed")]
+        public ISpread<int> FIdRemoved;
+
+        [Output("Saved Index")]
+        public ISpread<int> FId;
+
+        [Output("Saved Name")]
+        public ISpread<string> FOutput;
+
+        protected Dictionary<string, int> Data = new Dictionary<string, int>();
+        protected Dictionary<string, DateTime> Age = new Dictionary<string, DateTime>();
+        protected HashSet<int> FreeIndices = new HashSet<int>();
+
+        #endregion fields & pins
+
+        public void OnImportsSatisfied()
+        {
+            FCount.Changed += CountChanged;
+        }
+
+        protected void CountChanged(IDiffSpread<int> spread)
+        {
+            Reset();
+        }
+
+        protected void Reset()
+        {
+            Data.Clear();
+            Age.Clear();
+
+            FreeIndices.Clear();
+            for (int i = 0; i < FCount[0]; i++) FreeIndices.Add(i);
+        }
+
+
+        public void Evaluate(int SpreadMax)
+        {
+            FFormerId.SliceCount =
+            FIdFresh.SliceCount =
+            FIdRemoved.SliceCount = 0;
+
+            for (int i = 0; i < FRemove.SliceCount; i++)
+            {
+                var remove = FRemove[i];
+                remove = remove.Trim();
+                if (remove == "") continue;
+
+                if (Data.ContainsKey(remove))
+                {
+                    var index = Data[remove];
+                    FreeIndices.Add(index);
+                    Data.Remove(remove);
+                    Age.Remove(remove);
+                    FIdRemoved.Add(index);
+                }
+            }
+
+
+            for (int i = 0; i < FInput.SliceCount; i++)
+            {
+                var input = FInput[i].Trim();
+                if (input == "") continue;
+
+                int index;
+                if (!Data.ContainsKey(input))
+                {
+                    index = FreeIndices.DefaultIfEmpty(int.MinValue).First(); ;
+                    if (index == int.MinValue)
+                    {
+                        index = RemoveOldest();
+                        FIdRemoved.Add(index);
+                    }
+
+                    FreeIndices.Remove(index);
+                    FIdFresh.Add(index);
+                }
+                else index = Data[input];
+
+                Data[input] = index;
+                Age[input] = DateTime.Now;
+                FFormerId.Add(Data[input]);
+            }
+
+            var maxAge = FTimeOut[0];
+            var sessionEnabled = false;
+            if (maxAge != null && maxAge.Ticks > 0)
+                sessionEnabled = true;
+
+            foreach (var item in Age.Keys.ToArray())
+            {
+                var index = Data[item];
+                if (sessionEnabled && Age[item] + maxAge < DateTime.Now)
+                {
+                    FreeIndices.Add(index);
+                    Data.Remove(item);
+                    Age.Remove(item);
+                    FIdRemoved.Add(index);
+                }
+            }
+
+            var sorted = Data.Values.ToList();
+            sorted.Sort();
+            FId.AssignFrom(sorted);
+
+            FOutput.SliceCount = 0;
+            SpreadMax = sorted.Count;
+
+            FOutput.AssignFrom(
+                    from key in sorted
+                    from index in Data.Keys
+                    where Data[index] == key
+                    select index
+                    );
+        }
+
+        private int RemoveOldest()
+        {
+            var oldest = (
+                from id in Age.Keys
+                orderby Age[id]
+                select id
+            ).First();
+
+            var index = Data[oldest];
+            Data.Remove(oldest);
+            Age.Remove(oldest);
+            FreeIndices.Add(index);
+
+            return index;
+        }
+    }
+}
