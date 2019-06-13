19dc234d43d9e24513998da979f44ad55f405654
letmp <le_tmp@gmx.de>
2019-06-13 09:43:00 +0200

    [removed] obselete GeometryBuffer (DX11.Particles Instanced) node
    [updated] Kabsch Node is spreadable now

--- src/Nodes/DX11.Particles.Tools/KabschNode.cs
+++ src/Nodes/DX11.Particles.Tools/KabschNode.cs
@@ -17,10 +17,10 @@ namespace DX11.Particles.Tools
         #region fields & pins
 
         [Input("From ")]
-        public ISpread<Vector3D> FInputQ;
+        public ISpread<ISpread<Vector3D>> FInputQ;
 
         [Input("To ")]
-        public ISpread<Vector3D> FInputP;
+        public ISpread<ISpread<Vector3D>> FInputP;
 
         [Input("Enabled")]
         public ISpread<bool> FInputEnabled;
@@ -36,112 +36,113 @@ namespace DX11.Particles.Tools
         // see http://en.wikipedia.org/wiki/Kabsch_algorithm for a detailed explanation.
         public void Evaluate(int SpreadMax)
         {
-            FOutput.SliceCount = 1;
+            FOutput.SliceCount = SpreadMax;
             Matrix4x4 mOut;
 
-            if (FInputP.SliceCount > 0 && FInputQ.SliceCount > 0 && FInputEnabled[0])
+            if (FInputEnabled[0])
             {
-
-                // ======================== STEP 1 ========================
-                // translate both sets so that their centroids coincides with the origin
-                // of the coordinate system.
-
-                double[] meanP = new double[3] { 0.0, 0.0, 0.0 }; // mean of first point set
-                for (int i = 0; i < FInputP.SliceCount; i++)
+                for(int slice = 0; slice < SpreadMax; slice++)
                 {
-                    meanP[0] += FInputP[i].x;
-                    meanP[1] += FInputP[i].y;
-                    meanP[2] += FInputP[i].z;
-                }
-                meanP[0] /= FInputP.SliceCount;
-                meanP[1] /= FInputP.SliceCount;
-                meanP[2] /= FInputP.SliceCount;
-                double[][] centroidP = new double[3][] { new double[] { meanP[0] }, new double[] { meanP[1] }, new double[] { meanP[2] } };
-                GeneralMatrix mCentroidP = new GeneralMatrix(centroidP);
+                    // ======================== STEP 1 ========================
+                    // translate both sets so that their centroids coincides with the origin
+                    // of the coordinate system.
 
-                double[][] arrayP = new double[FInputP.SliceCount][];
-                for (int i = 0; i < FInputP.SliceCount; i++)
-                {
-                    arrayP[i] = new double[3];
-                    arrayP[i][0] = FInputP[i].x - meanP[0]; // subtract the mean values from the incoming pointset
-                    arrayP[i][1] = FInputP[i].y - meanP[1];
-                    arrayP[i][2] = FInputP[i].z - meanP[2];
-                }
-                // this is the matrix of the first pointset translated to the origin of the coordinate system
-                GeneralMatrix P = new GeneralMatrix(arrayP);
+                    double[] meanP = new double[3] { 0.0, 0.0, 0.0 }; // mean of first point set
+                    for (int i = 0; i < FInputP[slice].SliceCount; i++)
+                    {
+                        meanP[0] += FInputP[slice][i].x;
+                        meanP[1] += FInputP[slice][i].y;
+                        meanP[2] += FInputP[slice][i].z;
+                    }
+                    meanP[0] /= FInputP[slice].SliceCount;
+                    meanP[1] /= FInputP[slice].SliceCount;
+                    meanP[2] /= FInputP[slice].SliceCount;
 
-                double[] meanQ = new double[3] { 0.0, 0.0, 0.0 }; // mean of second point set
-                for (int i = 0; i < FInputQ.SliceCount; i++)
-                {
-                    meanQ[0] += FInputQ[i].x;
-                    meanQ[1] += FInputQ[i].y;
-                    meanQ[2] += FInputQ[i].z;
-                }
-                meanQ[0] /= FInputQ.SliceCount;
-                meanQ[1] /= FInputQ.SliceCount;
-                meanQ[2] /= FInputQ.SliceCount;
-                double[][] centroidQ = new double[3][] { new double[] { meanQ[0] }, new double[] { meanQ[1] }, new double[] { meanQ[2] } };
-                GeneralMatrix mCentroidQ = new GeneralMatrix(centroidQ);
+                    double[][] centroidP = new double[3][] { new double[] { meanP[0] }, new double[] { meanP[1] }, new double[] { meanP[2] } };
+                    GeneralMatrix mCentroidP = new GeneralMatrix(centroidP);
 
-                double[][] arrayQ = new double[FInputQ.SliceCount][];
-                for (int i = 0; i < FInputQ.SliceCount; i++)
-                {
-                    arrayQ[i] = new double[3];
-                    arrayQ[i][0] = FInputQ[i].x - meanQ[0]; // subtract the mean values from the incoming pointset
-                    arrayQ[i][1] = FInputQ[i].y - meanQ[1];
-                    arrayQ[i][2] = FInputQ[i].z - meanQ[2];
-                }
-                // this is the matrix of the second pointset translated to the origin of the coordinate system
-                GeneralMatrix Q = new GeneralMatrix(arrayQ);
+                    double[][] arrayP = new double[FInputP[slice].SliceCount][];
+                    for (int i = 0; i < FInputP[slice].SliceCount; i++)
+                    {
+                        arrayP[i] = new double[3];
+                        arrayP[i][0] = FInputP[slice][i].x - meanP[0]; // subtract the mean values from the incoming pointset
+                        arrayP[i][1] = FInputP[slice][i].y - meanP[1];
+                        arrayP[i][2] = FInputP[slice][i].z - meanP[2];
+                    }
+                    // this is the matrix of the first pointset translated to the origin of the coordinate system
+                    GeneralMatrix P = new GeneralMatrix(arrayP);
+
+                    double[] meanQ = new double[3] { 0.0, 0.0, 0.0 }; // mean of second point set
+                    for (int i = 0; i < FInputQ[slice].SliceCount; i++)
+                    {
+                        meanQ[0] += FInputQ[slice][i].x;
+                        meanQ[1] += FInputQ[slice][i].y;
+                        meanQ[2] += FInputQ[slice][i].z;
+                    }
+                    meanQ[0] /= FInputQ[slice].SliceCount;
+                    meanQ[1] /= FInputQ[slice].SliceCount;
+                    meanQ[2] /= FInputQ[slice].SliceCount;
+                    double[][] centroidQ = new double[3][] { new double[] { meanQ[0] }, new double[] { meanQ[1] }, new double[] { meanQ[2] } };
+                    GeneralMatrix mCentroidQ = new GeneralMatrix(centroidQ);
+
+                    double[][] arrayQ = new double[FInputQ[slice].SliceCount][];
+                    for (int i = 0; i < FInputQ[slice].SliceCount; i++)
+                    {
+                        arrayQ[i] = new double[3];
+                        arrayQ[i][0] = FInputQ[slice][i].x - meanQ[0]; // subtract the mean values from the incoming pointset
+                        arrayQ[i][1] = FInputQ[slice][i].y - meanQ[1];
+                        arrayQ[i][2] = FInputQ[slice][i].z - meanQ[2];
+                    }
+                    // this is the matrix of the second pointset translated to the origin of the coordinate system
+                    GeneralMatrix Q = new GeneralMatrix(arrayQ);
 
 
-                // ======================== STEP2 ========================
-                // calculate a covariance matrix A and compute the optimal rotation matrix
-                GeneralMatrix A = P.Transpose() * Q;
+                    // ======================== STEP2 ========================
+                    // calculate a covariance matrix A and compute the optimal rotation matrix
+                    GeneralMatrix A = P.Transpose() * Q;
 
-                SingularValueDecomposition svd = A.SVD();
-                GeneralMatrix U = svd.GetU();
-                GeneralMatrix V = svd.GetV();
+                    SingularValueDecomposition svd = A.SVD();
+                    GeneralMatrix U = svd.GetU();
+                    GeneralMatrix V = svd.GetV();
 
-                // calculate determinant for a special reflexion case.
-                double det = (V * U.Transpose()).Determinant();
-                double[][] arrayD = new double[3][]{ new double[]{1,0,0},
+                    // calculate determinant for a special reflexion case.
+                    double det = (V * U.Transpose()).Determinant();
+                    double[][] arrayD = new double[3][]{ new double[]{1,0,0},
                                                     new double[] {0,1,0},
                                                     new double[] {0,0,1}
                 };
-                arrayD[2][2] = det < 0 ? -1 : 1; // multiply 3rd column with -1 if determinant is < 0
-                GeneralMatrix D = new GeneralMatrix(arrayD);
+                    arrayD[2][2] = det < 0 ? -1 : 1; // multiply 3rd column with -1 if determinant is < 0
+                    GeneralMatrix D = new GeneralMatrix(arrayD);
 
-                // now we can compute the rotation matrix:
-                GeneralMatrix R = V * D * U.Transpose();
+                    // now we can compute the rotation matrix:
+                    GeneralMatrix R = V * D * U.Transpose();
 
-                // ======================== STEP3 ========================
-                // calculate the translation:
-                GeneralMatrix T = mCentroidP - R.Inverse() * mCentroidQ;
+                    // ======================== STEP3 ========================
+                    // calculate the translation:
+                    GeneralMatrix T = mCentroidP - R.Inverse() * mCentroidQ;
 
-                // ================== OUTPUT TRANSFORM ===================
+                    // ================== OUTPUT TRANSFORM ===================
 
-                mOut.m11 = (R.Array)[0][0];
-                mOut.m12 = (R.Array)[0][1];
-                mOut.m13 = (R.Array)[0][2];
-                mOut.m14 = 0;
-                mOut.m21 = (R.Array)[1][0];
-                mOut.m22 = (R.Array)[1][1];
-                mOut.m23 = (R.Array)[1][2];
-                mOut.m24 = 0;
-                mOut.m31 = (R.Array)[2][0];
-                mOut.m32 = (R.Array)[2][1];
-                mOut.m33 = (R.Array)[2][2];
-                mOut.m34 = 0;
-                mOut.m41 = (T.Array)[0][0];
-                mOut.m42 = (T.Array)[1][0];
-                mOut.m43 = (T.Array)[2][0];
-                mOut.m44 = 1;
-                FOutput[0] = mOut;
+                    mOut.m11 = (R.Array)[0][0];
+                    mOut.m12 = (R.Array)[0][1];
+                    mOut.m13 = (R.Array)[0][2];
+                    mOut.m14 = 0;
+                    mOut.m21 = (R.Array)[1][0];
+                    mOut.m22 = (R.Array)[1][1];
+                    mOut.m23 = (R.Array)[1][2];
+                    mOut.m24 = 0;
+                    mOut.m31 = (R.Array)[2][0];
+                    mOut.m32 = (R.Array)[2][1];
+                    mOut.m33 = (R.Array)[2][2];
+                    mOut.m34 = 0;
+                    mOut.m41 = (T.Array)[0][0];
+                    mOut.m42 = (T.Array)[1][0];
+                    mOut.m43 = (T.Array)[2][0];
+                    mOut.m44 = 1;
+                    FOutput[slice] = mOut;
+                }
             }
-
-
-            //FLogger.Log(LogType.Debug, T.Array[2][0].ToString());
+           
         }
     }
 }
